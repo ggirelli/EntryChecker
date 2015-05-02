bfdf4f4daf1c0b958a903de50eeaed12f1dc7131
ggirelli <gabriele@filopoe.it>
2015-05-02 12:16:19 +0200

    Basic structure.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Festa di Primavera 2015 - Get Inside</title>
+
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet"  type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet"  type="text/css" href="style.css" />
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/angular.min.js"></script>
+</head>
+<body>
+
+	<div id="wrap">
+		
+	</div>
+
+</body>
+</html>
